09941389e27bd08e2be82fcbd310c628bc29c1e4
Levente Kiss <levente.kiss@solarpunk.buzz>
2025-05-15 23:18:55 -0400

    fix: do not show auth screen after auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,17 +8,19 @@ import { useAuthStore } from "./store/auth";
 import { Key } from "@solarpunkltd/passkey-kit";
 
 export default function Home() {
+  const setShowAuth = useAuthStore((state) => state.setShowAuth);
   const showAuth = useAuthStore((state) => state.showAuth);
   const setKey = useAuthStore((state) => state.setKey);
 
-  const onCreate = (key: Key) => {
+  const onSetKey = (key: Key) => {
     setKey(key);
+    setShowAuth(false);
   };
 
   return (
     <div style={{ height: "100vh", width: "100vw" }}>
       {showAuth && (
-        <AuthProvider domain="http://localhost:5173" onCreate={onCreate} />
+        <AuthProvider domain="http://localhost:5173" onCreate={onSetKey} />
       )}
       <Header />
       <Welcome />
